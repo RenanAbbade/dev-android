afae25c28ad924740334f4c123dee9b55ccee111
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-12-29 18:24:38 -0300

    Incrementando a classe com um segundo constructor

--- Projetos-Kotlin/OOP.kt
+++ Projetos-Kotlin/OOP.kt
@@ -1,15 +1,26 @@
 fun main() {
 
     var pessoa: Pessoa = Pessoa("Renan", 1997)
-    print(pessoa.nome)
+    var pessoa2: Pessoa = Pessoa("Renan", 1997, 23)
 
     pessoa.saudacao()
+    pessoa2.saudacao()
 }
 
 
 class Pessoa(var nome: String, var anoNasc: Int){
 
+    var idade: Int? = null
+
+    //Segundo constructor
+    constructor(nome: String, anoNasc: Int, idade: Int) : this(nome,anoNasc){
+        this.idade = idade
+    }
+
     fun saudacao(){
-        println("Olá, meu nome é $nome e nasci em $anoNasc")
+        when(idade){
+            null -> println("Olá, meu nome é $nome e nasci em $anoNasc!")
+            else -> println("Olá, meu nome é $nome e nasci em $anoNasc, tenho $idade anos!")
+        }
     }
 }
