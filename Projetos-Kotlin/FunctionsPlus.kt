9b2fbc5620ca2f6dedca2068eafe8164b2d0dbb1
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-12-29 16:34:45 -0300

    Update FunctionsPlus.kt

--- Projetos-Kotlin/FunctionsPlus.kt
+++ Projetos-Kotlin/FunctionsPlus.kt
@@ -1,10 +1,11 @@
 fun main(){
     
-  endereco(cidade = "SP", estado = "SP")
-  endereco(rua="Alvaro Gomes",cidade = "SP", estado = "SP", numero = 105)
+    println(endereco(cidade = "SP", estado = "SP"))
+    println(endereco(rua="Alvaro Gomes",cidade = "SP", estado = "SP", numero = 105))
 }
 
-//Os parametros com atribuição referem-se a valores opcionais. Os sem atribuição são valores default, se não houver passagem eles assumem os valores dafault.
-fun endereco(rua: String = "", cidade: String, estado: String, numero: Int = 0){
-    println("Rua: $rua, Cidade: $cidade, Estado: $estado, Numero: $numero")
+//Os parametros com atribuição referem-se a valores opcionais.
+//
+fun endereco(rua: String = "", cidade: String, estado: String, numero: Int = 0) : String {
+    return "Rua: $rua, Cidade: $cidade, Estado: $estado, Numero: $numero"
 }
