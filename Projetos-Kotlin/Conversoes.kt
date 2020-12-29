de64f196961989d3cf0e58b9d26c5ab560db1ff1
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-12-29 16:10:34 -0300

    Update Conversoes.kt

--- Projetos-Kotlin/Conversoes.kt
+++ Projetos-Kotlin/Conversoes.kt
@@ -1,1 +1,15 @@
 
+
+fun main(args: Array<String>) {
+ 
+    val l1: Long = 5454212158
+    val n1: Int = l1.toInt()
+    val n2: Double = l1.toDouble()
+    val n3: Short = l1.toShort()
+    val n4: String = l1.toString()
+    println(n1)
+    println(n2)
+    println(n3)
+    println(n4)
+    
+}
