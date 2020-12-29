e5e9f021f04b135c96dbc6f42a87eb1145fe899a
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-12-29 18:41:35 -0300

    Getters & Setters padrões e customizados

--- Projetos-Kotlin/OOP.kt
+++ Projetos-Kotlin/OOP.kt
@@ -5,22 +5,41 @@ fun main() {
 
     pessoa.saudacao()
     pessoa2.saudacao()
+
+    pessoa.codigoPessoa = 59069843
+
+    println(pessoa.codigoPessoa)
+
 }
 
 
-class Pessoa(var nome: String, var anoNasc: Int){
+class Pessoa(var nome: String, var anoNasc: Int) {
 
     var idade: Int? = null
 
     //Segundo constructor
-    constructor(nome: String, anoNasc: Int, idade: Int) : this(nome,anoNasc){
+    constructor(nome: String, anoNasc: Int, idade: Int) : this(nome, anoNasc) {
         this.idade = idade
     }
 
-    fun saudacao(){
-        when(idade){
-            null -> println("Olá, meu nome é $nome e nasci em $anoNasc!")
-            else -> println("Olá, meu nome é $nome e nasci em $anoNasc, tenho $idade anos!")
+    //Os getters e setters padrão (Boilerplate) são criados por baixo do pano no Kotlin.
+
+    //Se for necessário criar getters e setters especificos, podemos fazer da seguinte maneira:
+    var codigoPessoa: Int = 0
+        get() {
+            print("Get de codigoPessoa foi chamado ")
+            return field
         }
+        set(value) {
+            print("Set de codigoPessoa foi chamado ")
+            field = value
+        }
+
+    fun saudacao() {
+        when (idade) {
+            null -> println("Olá, meu nome é $nome e nasci em $anoNasc")
+            else -> println("Olá, meu nome é $nome e nasci em $anoNasc, tenho $idade anos")
+        }
+
     }
 }
