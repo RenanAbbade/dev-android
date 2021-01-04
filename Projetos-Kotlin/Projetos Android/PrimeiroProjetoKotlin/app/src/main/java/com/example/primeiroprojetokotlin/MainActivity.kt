720ca639332764622dd088d3793a461974b1558b
Renan Abbade <renanabbade1997@gmail.com>
2021-01-04 12:38:13 -0300

    Criação do layout - App gasto viagem

--- Projetos-Kotlin/Projetos Android/PrimeiroProjetoKotlin/app/src/main/java/com/example/primeiroprojetokotlin/MainActivity.kt
+++ Projetos-Kotlin/Projetos Android/PrimeiroProjetoKotlin/app/src/main/java/com/example/primeiroprojetokotlin/MainActivity.kt
@@ -5,18 +5,12 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun cliqueBotao(view: View){
-        Log.i("BOTAO","Botão foi pressioado")
-        var texto = findViewById<TextView>(R.id.textView)
-        texto.setText("Texto Alterado!")
-
     }
 }
