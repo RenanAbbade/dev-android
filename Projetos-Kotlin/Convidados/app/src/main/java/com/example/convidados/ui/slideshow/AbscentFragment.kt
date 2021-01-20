e9728c0588549378706b0b85b71b64ba785e922e
Renan Abbade <renanabbade1997@gmail.com>
2021-01-20 15:29:01 -0300

    App Convidados init

---
+++ Projetos-Kotlin/Convidados/app/src/main/java/com/example/convidados/ui/slideshow/AbscentFragment.kt
@@ -0,0 +1,31 @@
+package com.example.convidados.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.convidados.R
+
+class AbscentFragment : Fragment() {
+
+    private lateinit var abscentViewModel: AbscentViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        abscentViewModel =
+                ViewModelProviders.of(this).get(AbscentViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val textView: TextView = root.findViewById(R.id.text_slideshow)
+        abscentViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
